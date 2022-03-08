172b04e448a63ed70259d76bfd09f5d20ce74778
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-08 16:07:29 +0600

    8.6

---
+++ lib/ui/login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class Login_screen extends StatelessWidget {
+  const Login_screen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("This is Login Page")),
+      
+    );
+  }
+}
