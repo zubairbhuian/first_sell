3272eda35224ba2a79bd298c3d6d8938fe111366
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-09 21:51:26 +0600

    9.6

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -11,6 +11,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -102,7 +104,98 @@ class _LoginScreenState extends State<LoginScreen> {
                           ),
                         ],
                       ),
-                      
+                      Row(
+                        children: [
+                          Container(
+                            height: 48.h,
+                            width: 41.w,
+                            decoration: BoxDecoration(
+                                color: AppColors.deep_orange,
+                                borderRadius: BorderRadius.circular(12.r)),
+                            child: Center(
+                              child: Icon(
+                                Icons.lock_outline,
+                                color: Colors.white,
+                                size: 20.w,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.w,
+                          ),
+                          Expanded(
+                            child: TextField(
+                              controller: _passwordController,
+                              obscureText: _obscureText,
+                              decoration: InputDecoration(
+                                hintText: "password must be 6 character",
+                                hintStyle: TextStyle(
+                                  fontSize: 14.sp,
+                                  color: Color(0xFF414041),
+                                ),
+                                labelText: 'PASSWORD',
+                                labelStyle: TextStyle(
+                                  fontSize: 15.sp,
+                                  color: AppColors.deep_orange,
+                                ),
+                                suffixIcon: _obscureText == true
+                                    ? IconButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            _obscureText = false;
+                                          });
+                                        },
+                                        icon: Icon(
+                                          Icons.remove_red_eye,
+                                          size: 20.w,
+                                        ))
+                                    : IconButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            _obscureText = true;
+                                          });
+                                        },
+                                        icon: Icon(
+                                          Icons.visibility_off,
+                                          size: 20.w,
+                                        )),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      ElevatedButton(
+                        
+                          onPressed: () {}, child: const Text("Sign In")),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Wrap(
+                        children: [
+                          Text(
+                            "Don't have an account?",
+                            style: TextStyle(
+                              fontSize: 13.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFFBBBBBB),
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              " Sign Up",
+                              style: TextStyle(
+                                fontSize: 13.sp,
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.deep_orange,
+                              ),
+                            ),
+                            onTap: () {},
+                          )
+                        ],
+                      )
                     ],
                   ),
                 ),
