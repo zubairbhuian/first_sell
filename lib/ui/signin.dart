5423d5fa42d0186615034712732bba4e0bfc2a8e
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-09 22:51:12 +0600

    9.10

---
+++ lib/ui/signin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class SignIn extends StatelessWidget {
+  const SignIn({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child:Text("demo") ,
+      ),
+    );
+  }
+}
