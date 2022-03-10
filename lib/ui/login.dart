1c30c6aa3af5397a8dc8a8d349fc19b61ab4575a
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-10 16:56:13 +0600

    10.3

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:first_sell/const/app_color.dart';
+import 'package:first_sell/ui/homeScreen.dart';
 import 'package:first_sell/ui/signup.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -12,9 +15,38 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController  _emailController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
   bool _obscureText = true;
+  signIn() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: _emailController.text, password: _passwordController.text);
+      var authCredentional = userCredential.user;
+      print(authCredentional!.uid);
+      if (authCredentional.uid.isNotEmpty) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+      } else {
+        Fluttertoast.showToast(
+            msg: "Some thing is rong", backgroundColor: AppColors.deep_orange);
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        Fluttertoast.showToast(
+            msg: "No user found for that email.",
+            backgroundColor: AppColors.deep_orange);
+      } else if (e.code == 'wrong-password') {
+        Fluttertoast.showToast(
+            msg: "Wrong password provided for that user.",
+            backgroundColor: AppColors.deep_orange);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -173,7 +205,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         width: 1.sw,
                         height: 56.h,
                         child: ElevatedButton(
-                          onPressed: () {},
+                          onPressed: () => signIn(),
                           child: const Text("Sign In"),
                           style: ElevatedButton.styleFrom(
                               primary: AppColors.deep_orange),
